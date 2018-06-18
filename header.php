d5dcae0aea4fc322ca19d1477e1e738b9fd157cd
Nonso Mgbechi <mgbechinonso@gmail.com>
2018-06-18 08:36:17 +0100

    add legomake index and current dependencies

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title><?=$title;?></title>
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="assets/css/style1.css" rel="stylesheet">
+</head>
+<body class= "container-fluid">
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<a class="navbar-brand" href="#" title="Build amazing products affordably">Legomake</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+				<ul class="navbar-nav mx-auto mt-2 mt-lg-0">
+					<li class="nav-item active dropdown mr-2">
+                        <a class="nav-link" href="#" data-toggle="dropdown" id="navbarDropdown" title="Learn about our services">
+							Services<span class="sr-only">(current)</span>
+						</a>
+						<div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Web development</a>
+							<a class="dropdown-item" href="#">Mobile app development</a>
+							<a class="dropdown-item" href="#">Graphic design</a>
+						</div>
+					</li>
+					<li class="nav-item mr-2">
+						<a class="nav-link" href="#" title="See our projects">Portfolio</a>
+					</li>
+					<li class="nav-item mr-2">
+						<a class="nav-link" href="#" title="Get our estimate for your project">Estimate Project</a>
+					</li>
+								
+				</ul>	
+				<div class="dropdown-divider"></div>
+				<ul class="navbar-nav mr-2 mt-2 mt-lg-0">
+					<li class="nav-item">
+						<a class="btn btn-primary" href="#" title="Send us a message :)">Contact</a>
+					</li>	
+				</ul>			
+			</div>
+			
+		</nav>
+	</header>
