017e2a4320380ed632302f9e97fc238574428190
Nonso Mgbechi <mgbechinonso@gmail.com>
2018-06-22 00:24:32 +0100

    created footer.php file and included it in index file

---
+++ footer.php
@@ -0,0 +1,64 @@
+<footer>
+		
+		<div  id="site-footer">
+			<div class="row">
+					<div class="col-md-4 col-sm-6">
+						<div class="margin-footer col-div twit">
+							<h3>Twitter Feeds</h3>
+								<blockquote class="twitter-tweet"><p lang="en" dir="ltr">We are live on social media now. Feel free to follow us <a href="https://twitter.com/Linkaidit?ref_src=twsrc%5Etfw">@Linkaidit</a></p>&mdash; Linkaid (@Linkaidit) <a href="https://twitter.com/Linkaidit/status/922606216492343296?ref_src=twsrc%5Etfw">October 23, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>			
+							
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="margin-footer">
+							<div class="col-div">
+								<h3>Get In Touch</h3>
+								<div>
+									<div>
+										<span class="linkaid">Legomake IT Services</span><br>
+										<span>28 Apena Street, </span><br>
+										<span>Ojuelegba, Surulere,</span><br>										
+										<span>Lagos, Nigeria</span>
+									</div>
+									<div class="margin-footer">
+										<span>+(234) 8141189211</span><br>
+										<span>hello@legomake.com</span>
+									</div>
+								</div>
+								<div>
+									<div id="social-pad">
+											<a href="#" title="Connect with us on facebook"><i id="social-fb" class="fa fa-facebook fa-2x social text-center"></i></a>
+											<a href="#" title="Connect with us on twitter"><i id="social-tw" class="fa fa-twitter fa-2x social text-center"></i></a>
+											<a href="#" title="Connect with us on google+"><i id="social-gp" class="fa fa-google-plus fa-2x social text-center"></i></a>
+											<a href="#" title="Connect with us on instagram"><i id="social-em" class="fa fa-instagram fa-2x social text-center"></i></a>
+									</div>
+									
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="margin-footer">
+							<div class="margin-footer col-div">
+								<ul id="footer-nav" class="margin-footer">
+									<li><a href="../legomake" class="a_font">Home</a></li>
+									<li><a href="#" class="a_font">Services</a></li>
+									<li><a href="#" class="a_font">Portfolio</a></li>
+									<li><a href="#" class="a_font">Estimate Project</a></li>
+									<li><a href="#" class="a_font">Contact</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			
+			<div id="pcmn_div">
+				<p>Legomake IT Services &copy;<?=date("Y");?>. All Rights Reserved.</p>
+			</div>
+		</div>
+	
+	</footer>
+
+	<script src="assets/js/jquery.min.js"></script>	
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.js"></script>
